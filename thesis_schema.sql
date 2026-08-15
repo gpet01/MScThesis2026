2fae10e182cf7c8b64bd1205a73e4a3e7ca16211
giorgospetmezas <giorgospetm01@gmail.com>
2026-08-15 13:53:06 +0300

    Added more tables

--- thesis_schema.sql
+++ thesis_schema.sql
@@ -2,3 +2,31 @@ CREATE TABLE `categories` (
     `id` int(11) NOT NULL,
     `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `orders` (
+    `id` int(11) NOT NULL,
+    `user_id` int(11) NOT NULL,
+    `order_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `order_items` (
+    `id` int(11) NOT NULL,
+    `order_id` int(11) NOT NULL,
+    `product_id` int(11) NOT NULL,
+    `quantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `products` (
+    `id` int(11) NOT NULL,
+    `name` varchar(150) NOT NULL,
+    `category_id` int(11) DEFAULT NULL,
+    `price` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `users` (
+    `id` int(11) NOT NULL,
+    `name` varchar(100) NOT NULL,
+    `age` int(11) DEFAULT NULL,
+    `email` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
