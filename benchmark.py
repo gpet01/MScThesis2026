4b08a5bfa106f48b0cc45043229da0c2483bc584
giorgospetmezas <giorgospetm01@gmail.com>
2026-09-07 19:43:59 +0300

    Added order items retrieval

--- benchmark.py
+++ benchmark.py
@@ -44,12 +44,59 @@ def avg_mysql_users():
         times.append((time.perf_counter() - start) * 1000)
     return statistics.mean(times), statistics.stdev(times)
 
+def avg_mysql_order():
+    for _ in range(WARMUP):
+        cursor.execute("SELECT * FROM orders WHERE id = %s", (random.randint(1, NUM_ORDERS),))
+        cursor.fetchall()
+    times = []
+    for _ in range(REPEATS):
+        order_id = random.randint(1, NUM_ORDERS)
+        start = time.perf_counter()
+        cursor.execute("SELECT * FROM orders WHERE id = %s", (order_id,))
+        cursor.fetchall()
+        times.append((time.perf_counter() - start) * 1000)
+    return statistics.mean(times), statistics.stdev(times)
+
+
+def avg_mysql_order_items():
+    for _ in range(WARMUP):
+        cursor.execute("""
+            SELECT p.name
+            FROM order_items oi
+            JOIN products p ON oi.product_id = p.id
+            WHERE oi.order_id = %s
+        """, (random.randint(1, NUM_ORDERS),)
+        )
+        cursor.fetchall()
+    times = []
+    for _ in range(REPEATS):
+        order_id = random.randint(1, NUM_ORDERS)
+        start = time.perf_counter()
+        cursor.execute("""
+            SELECT p.name
+            FROM order_items oi
+            JOIN products p ON oi.product_id = p.id
+            WHERE oi.order_id = %s
+        """, (order_id,) )
+        cursor.fetchall()
+        times.append( (time.perf_counter() - start) * 1000)
+    return statistics.mean(times), statistics.stdev(times)
+
+
+
+
+# -------------------------
+# Redis Benchmarks
+# -------------------------
+
 
 #--------------------------
 # Run Benchmarks
 #--------------------------
 
 mysql_user = avg_mysql_users()
+mysql_order = avg_mysql_order()
+mysql_items = avg_mysql_order_items()
 
 
 
@@ -61,3 +108,5 @@ mysql_user = avg_mysql_users()
 
 print("\n=== MySQL Average Response Times (mean ±stdev) ===")
 print(f"User retrieval: {mysql_user[0]:.3f} ms ± {mysql_user[1]:.3f} ms")
+print(f"Order retrieval: {mysql_order[0]:.3f} ms ± {mysql_order[1]:.3f} ms")
+print(f"Order items retrieval: {mysql_items[0]:.3f} ms ± {mysql_items[1]:.3f} ms")
